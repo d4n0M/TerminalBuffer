00663f353127422e0eb09a33406457c2d78c9bb2
d4n0M <danielceresna@gmail.com>
2026-02-16 22:37:15 +0100

    Initial Structure

---
+++ src/main/java/org/example/Main.java
@@ -0,0 +1,10 @@
+package org.example;
+
+import org.example.buffer.TerminalBuffer;
+import org.example.model.*;
+
+public class Main {
+    static void main() {
+
+    }
+}
---
+++ src/main/java/org/example/buffer/TerminalBuffer.java
@@ -0,0 +1,5 @@
+package org.example.buffer;
+
+public class TerminalBuffer {
+
+}
