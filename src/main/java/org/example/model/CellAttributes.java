07bfdba01b578e7ae25bf809754f0bbe40c72efa
d4n0M <danielceresna@gmail.com>
2026-02-17 20:24:13 +0100

    Implemented CellAttributes class

--- src/main/java/org/example/model/CellAttributes.java
+++ src/main/java/org/example/model/CellAttributes.java
@@ -1,4 +1,57 @@
 package org.example.model;
 
 public class CellAttributes {
+    private Color foregroundColor;
+    private Color backgroundColor;
+    private StyleFlags style;
+
+    public CellAttributes(Color foregroundColor, Color backgroundColor, StyleFlags style){
+        this.foregroundColor = foregroundColor;
+        this.backgroundColor = backgroundColor;
+        this.style = style;
+    }
+
+    public CellAttributes(){
+        this(Color.DEFAULT, Color.DEFAULT, new StyleFlags());
+    }
+
+    public CellAttributes(CellAttributes attributes){
+        this(attributes.foregroundColor, attributes.backgroundColor, attributes.style);
+    }
+
+    public void reset(){
+        foregroundColor = Color.DEFAULT;
+        backgroundColor = Color.DEFAULT;
+        style.reset();
+    }
+
+    public void applyToCell(Cell cell){
+        cell.setForegroundColor(foregroundColor);
+        cell.setBackgroundColor(backgroundColor);
+        cell.setStyleFlags(style);
+    }
+
+    public Color getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(Color foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public Color getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(Color backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+
+    public StyleFlags getStyle() {
+        return style;
+    }
+
+    public void setStyle(StyleFlags style) {
+        this.style = style;
+    }
 }
