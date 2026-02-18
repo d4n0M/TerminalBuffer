c66c700154f90e8bd44741f533f34f01d698e02d
d4n0M <danielceresna@gmail.com>
2026-02-18 15:36:59 +0100

    Implemented basic structure for TerminalBufferTests

--- src/test/java/org/example/buffer/TerminalBufferTest.java
+++ src/test/java/org/example/buffer/TerminalBufferTest.java
@@ -1,1 +1,14 @@
 package org.example.buffer;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+public class TerminalBufferTest {
+
+    @Test
+    void canInstantiate() {
+        TerminalBuffer buffer = new TerminalBuffer();
+        assertNotNull(buffer);
+    }
+}
